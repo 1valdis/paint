87ecdb0395dd90ebc599817b5bbf60143816836e
Vladislav Gooba <vladhuba@ukr.net>
2018-12-24 13:12:51 +0200

    update all dependencies

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import './Image.css'
 
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 import classNames from 'classnames'
 
 import { selectInstrument } from '../instruments/actions'
+import { changeImage } from '../App/actions'
 
 class Image extends Component {
   render () {
     return (
       <nav className='image'>
-        <button className={classNames('select', {select_active: this.props.instrument === 'selection'})} onClick={this.handleClick}>
+        <button
+          className={classNames('select', {
+            select_active: this.props.instrument === 'selection'
+          })}
+          onClick={this.handleClick}
+        >
           <svg viewBox='0 0 15 10'>
             <rect
               width='15'
@@ -26,8 +32,13 @@ class Image extends Component {
           </svg>
           <footer>Выделить</footer>
         </button>
-        <div className="side-buttons">
-          <button>Обрезать</button>
+        <div className='side-buttons'>
+          <button
+            onClick={this.handleClipClick}
+            disabled={!(this.props.selection && this.props.selection.coords)}
+          >
+            Обрезать
+          </button>
           <button>Изменить размер</button>
           <button>Повернуть</button>
         </div>
@@ -37,13 +48,38 @@ class Image extends Component {
   handleClick = () => {
     this.props.selectInstrument('selection')
   }
+  handleClipClick = () => {
+    if (!(this.props.selection && this.props.selection.coords)) {
+      return
+    }
+    const newCanvas = document.createElement('canvas')
+    const newCtx = newCanvas.getContext('2d')
+    ;({
+      width: newCanvas.width,
+      height: newCanvas.height
+    } = this.props.selection.coords)
+
+    newCtx.putImageData(this.props.image, 0, 0)
+    this.props.changeImage(
+      newCtx.getImageData(
+        this.props.selection.coords.left,
+        this.props.selection.coords.top,
+        this.props.selection.coords.width,
+        this.props.selection.coords.height
+      )
+    )
+    this.props.selectInstrument('selection')
+  }
 }
 
 const mapStateToProps = state => ({
-  instrument: state.instruments.instrument
+  image: state.image.data,
+  instrument: state.instruments.instrument,
+  selection: state.instruments.selection
 })
 const mapDispatchToProps = dispatch => ({
-  selectInstrument: instrument => dispatch(selectInstrument(instrument))
+  selectInstrument: instrument => dispatch(selectInstrument(instrument)),
+  changeImage: imageData => dispatch(changeImage(imageData))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Image)
---
+++ src/components/Thickness/Thickness.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './Image.css'
+
+import { connect } from 'react-redux'
+
+import { changeImage } from '../App/actions'
+
+class Image extends Component {
+  
+}
+
+const mapStateToProps = state => ({
+  image: state.image.data,
+  instrument: state.instruments.instrument,
+  selection: state.instruments.selection
+})
+const mapDispatchToProps = dispatch => ({
+  changeThickness = 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Image)
