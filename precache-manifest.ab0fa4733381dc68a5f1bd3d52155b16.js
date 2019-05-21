7d9cdfcdc14e2c6d870443da676832d3484adcbe
1valdis <vladhuba@gmail.com>
2019-05-21 22:04:44 +0300

    Updates

---
+++ precache-manifest.ab0fa4733381dc68a5f1bd3d52155b16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8732f12f3931a5bad9a61839d436e069",
+    "url": "/paint/index.html"
+  },
+  {
+    "revision": "21dd756b0fed08e3f792",
+    "url": "/paint/static/css/main.4ea74e05.chunk.css"
+  },
+  {
+    "revision": "2d5377c4150f6ebca009",
+    "url": "/paint/static/js/2.236cb1a8.chunk.js"
+  },
+  {
+    "revision": "21dd756b0fed08e3f792",
+    "url": "/paint/static/js/main.43f89492.chunk.js"
+  },
+  {
+    "revision": "39ecc2dbfe1335556c68",
+    "url": "/paint/static/js/runtime~main.098c9039.js"
+  },
+  {
+    "revision": "e9f5edd1b0d682bed17d922857d9b60c",
+    "url": "/paint/static/media/color-input-icon.e9f5edd1.png"
+  },
+  {
+    "revision": "be0d253d382cca758128bfc23c319ecc",
+    "url": "/paint/static/media/crosshair-monochrome.be0d253d.svg"
+  },
+  {
+    "revision": "f861943cb4672cd3e832a4e239c2dfb1",
+    "url": "/paint/static/media/dropper_colorful.f861943c.svg"
+  },
+  {
+    "revision": "e172e5fc6a0f3db394aa33c67a3b347a",
+    "url": "/paint/static/media/dropper_monochrome.e172e5fc.svg"
+  },
+  {
+    "revision": "8b3022f29a608659ca449c532c727437",
+    "url": "/paint/static/media/eraser_colorful.8b3022f2.svg"
+  },
+  {
+    "revision": "7c613b9d64badca21e646e309f3d00d9",
+    "url": "/paint/static/media/fill_colorful.7c613b9d.svg"
+  },
+  {
+    "revision": "a111789b584f8276a5dad7b7042ddb6b",
+    "url": "/paint/static/media/pen_colorful.a111789b.svg"
+  },
+  {
+    "revision": "70d7efe5080a8950bc1028e7f52b2ecb",
+    "url": "/paint/static/media/zoom_colorful.70d7efe5.svg"
+  }
+]);
