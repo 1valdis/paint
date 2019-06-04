7230d3a7f0405f303e11b4fae40fe65bbf42ccb0
1valdis <vladhuba@gmail.com>
2019-06-04 19:08:04 +0300

    better display on mac, redux devtools support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import Paint, { rootReducer } from './components/App/App'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 render(
   <StrictMode>
