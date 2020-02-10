89341b36bfa027963eb573f5be3a7f30eb91b9ca
1valdis <vladhuba@gmail.com>
2020-02-10 11:48:54 +0200

    split out canvas resizer

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -2,25 +2,15 @@ import React, { PureComponent, createRef, RefObject } from 'react'
 
 import { connect } from 'react-redux'
 
-import {
-  changeImage,
-  // Color,
-  Action
-  // changeInstrument,
-  // Instruments
-} from '../../actions'
-
-import { Resizer, ResizerMode } from '../Resizer/Resizer'
+import { StoreState } from '../../reducers'
+import { CanvasResizer } from '../CanvasResizer/CanvasResizer'
 // import { CanvasEditor } from '../CanvasEditor/CanvasEditor'
 
 import './Canvas.css'
-import { StoreState } from '../../reducers'
-import { ThunkDispatch } from 'redux-thunk'
 // import { InstrumentStoreState } from '../../reducers/instruments'
 
 export interface CanvasProps {
   imageData: ImageData
-  changeImage: (imageData: ImageData) => void
 }
 
 class _Canvas extends PureComponent<CanvasProps> {
@@ -32,26 +22,16 @@ class _Canvas extends PureComponent<CanvasProps> {
         <canvas
           ref={this.canvas}
           className="canvas"
-          width={this.props.imageData ? this.props.imageData.width : 0}
-          height={this.props.imageData ? this.props.imageData.height : 0}
+          width={this.props.imageData.width}
+          height={this.props.imageData.height}
         />
         <div
           className="canvas-upper-layer"
           style={{
-            width: this.props.imageData ? this.props.imageData.width : 0,
-            height: this.props.imageData ? this.props.imageData.height : 0
+            width: this.props.imageData.width,
+            height: this.props.imageData.height
           }}>
-          <Resizer
-            mode={ResizerMode.canvas}
-            onResizeEnd={this.onResize}
-            // onResizing={() =>
-            //   changeInstrument({ instrument: this.props.selectedInstrument })
-            // }
-            width={this.props.imageData ? this.props.imageData.width : 0}
-            height={this.props.imageData ? this.props.imageData.height : 0}
-            top={0}
-            left={0}
-          />
+          <CanvasResizer />
           {/* <CanvasEditor /> */}
         </div>
       </div>
@@ -60,7 +40,7 @@ class _Canvas extends PureComponent<CanvasProps> {
 
   updateCanvas = () => {
     const canvas = this.canvas.current
-    if (canvas != null && this.props.imageData != null) {
+    if (canvas != null) {
       ;[canvas.width, canvas.height] = [
         this.props.imageData.width,
         this.props.imageData.height
@@ -68,23 +48,6 @@ class _Canvas extends PureComponent<CanvasProps> {
       const ctx = canvas.getContext('2d')
       if (!ctx) throw new Error("Coudn't acquire context")
       ctx.putImageData(this.props.imageData, 0, 0)
-    }
-  }
-
-  onResize = (top: number, left: number, toWidth: number, toHeight: number) => {
-    if (
-      this.props.imageData.width !== toWidth ||
-      this.props.imageData.height !== toHeight
-    ) {
-      const newCanvas = document.createElement('canvas')
-      newCanvas.width = toWidth
-      newCanvas.height = toHeight
-      const newCtx = newCanvas.getContext('2d')
-      if (!newCtx) throw new Error("Coudn't acquire context")
-      // newCtx.fillStyle = `rgb(${this.props.secondaryColor.r},${this.props.secondaryColor.g},${this.props.secondaryColor.b})`
-      newCtx.fillRect(0, 0, toWidth, toHeight)
-      newCtx.putImageData(this.props.imageData, 0, 0)
-      this.props.changeImage(newCtx.getImageData(0, 0, toWidth, toHeight))
     }
   }
 
@@ -99,16 +62,6 @@ class _Canvas extends PureComponent<CanvasProps> {
 
 const mapStateToProps = (state: StoreState) => ({
   imageData: state.image.imageData
-  // secondaryColor: state.colors.list[state.colors.secondary],
-  // selectedInstrument: state.instruments.instrument
 })
 
-const mapDispatchToProps = (
-  dispatch: ThunkDispatch<StoreState, undefined, Action>
-) => ({
-  changeImage: (imageData: ImageData) => dispatch(changeImage(imageData))
-  // changeInstrument: (instrumentData: InstrumentStoreState) =>
-  //   dispatch(changeInstrument(instrumentData))
-})
-
-export const Canvas = connect(mapStateToProps, mapDispatchToProps)(_Canvas)
+export const Canvas = connect(mapStateToProps)(_Canvas)
---
+++ src/components/CanvasResizer/CanvasResizer.tsx
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react'
+import { Resizer } from '../Resizer/Resizer'
+import { StoreState } from '../../reducers'
+import { ThunkDispatch } from 'redux-thunk'
+import { changeImage, Action } from '../../actions'
+import { connect } from 'react-redux'
+
+export interface CanvasResizerProps {
+  imageData: ImageData
+  changeImage(imageData: ImageData): void
+}
+
+class _CanvasResizer extends PureComponent<CanvasResizerProps> {
+  render() {
+    return (
+      <Resizer
+        mode="canvas"
+        onResizeEnd={this.onResize}
+        // onResizing={() =>
+        //   changeInstrument({ instrument: this.props.selectedInstrument })
+        // }
+        width={this.props.imageData.width}
+        height={this.props.imageData.height}
+        top={0}
+        left={0}
+      />
+    )
+  }
+
+  onResize = (top: number, left: number, toWidth: number, toHeight: number) => {
+    if (
+      this.props.imageData.width !== toWidth ||
+      this.props.imageData.height !== toHeight
+    ) {
+      const newCanvas = document.createElement('canvas')
+      newCanvas.width = toWidth
+      newCanvas.height = toHeight
+      const newCtx = newCanvas.getContext('2d')
+      if (!newCtx) throw new Error("Coudn't acquire context")
+      // newCtx.fillStyle = `rgb(${this.props.secondaryColor.r},${this.props.secondaryColor.g},${this.props.secondaryColor.b})`
+      newCtx.fillRect(0, 0, toWidth, toHeight)
+      newCtx.putImageData(this.props.imageData, 0, 0)
+      this.props.changeImage(newCtx.getImageData(0, 0, toWidth, toHeight))
+    }
+  }
+}
+
+const mapStateToProps = (state: StoreState) => ({
+  imageData: state.image.imageData
+  // secondaryColor: state.colors.list[state.colors.secondary],
+  // selectedInstrument: state.instruments.instrument
+})
+
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<StoreState, undefined, Action>
+) => ({
+  changeImage: (imageData: ImageData) => dispatch(changeImage(imageData))
+  // changeInstrument: (instrumentData: InstrumentStoreState) =>
+  //   dispatch(changeInstrument(instrumentData))
+})
+
+export const CanvasResizer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_CanvasResizer)
